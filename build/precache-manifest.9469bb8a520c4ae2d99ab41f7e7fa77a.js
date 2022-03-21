fa79997513835d2f09ff71495a28c8efbbb85081
Apoorv Tiwari <apoorv.tiwari@oriserve.com>
2022-03-21 14:33:33 +0530

    Fixed redux devtools

---
+++ build/precache-manifest.9469bb8a520c4ae2d99ab41f7e7fa77a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cde184aec8b31fba590424418c91c1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05bb408fc41b720b2091",
+    "url": "/static/css/2.7a908399.chunk.css"
+  },
+  {
+    "revision": "faeb95aba17e063f506d",
+    "url": "/static/css/main.af1671eb.chunk.css"
+  },
+  {
+    "revision": "05bb408fc41b720b2091",
+    "url": "/static/js/2.6f6e896c.chunk.js"
+  },
+  {
+    "revision": "b9101d81cbf7b52e7f615fa8cbcafc6b",
+    "url": "/static/js/2.6f6e896c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faeb95aba17e063f506d",
+    "url": "/static/js/main.c28c66ca.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "2c95a80b79f694735affb410f3843809",
+    "url": "/static/media/goldline@2x.2c95a80b.png"
+  },
+  {
+    "revision": "152eb33f3d4791579c48d923239a3c51",
+    "url": "/static/media/ideaa@2x.152eb33f.png"
+  },
+  {
+    "revision": "93f8b0c6985d984787d8063af0d284d7",
+    "url": "/static/media/img1.93f8b0c6.png"
+  },
+  {
+    "revision": "fded0c97c06617a9f8eb6dfab5f4450b",
+    "url": "/static/media/kanba@2x.fded0c97.png"
+  },
+  {
+    "revision": "6b88ea6ec6147ea14631a2145dfbff40",
+    "url": "/static/media/lighting@2x.6b88ea6e.png"
+  },
+  {
+    "revision": "5cfa82b643e23bd0fc06b552cb392e03",
+    "url": "/static/media/liva@2x.5cfa82b6.png"
+  },
+  {
+    "revision": "35effda36507e94d81e3ee0cf2a317bd",
+    "url": "/static/media/solaytic@2x.35effda3.png"
+  },
+  {
+    "revision": "25af4e9ee1b900548942fa26b3501b10",
+    "url": "/static/media/velocity-9@2x.25af4e9e.png"
+  },
+  {
+    "revision": "39f2c7186dff7e89d74b381523df6b4c",
+    "url": "/static/media/ztos@2x.39f2c718.png"
+  }
+]);
